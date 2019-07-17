486537b082b450b4d4a266c833a80c069b17629e
RostyslavAntonyshyn <rostyslav.antonyshyn@gmail.com>
2019-07-17 18:44:09 +0300

    added some tests

--- chainntnfs/lightwalletnotify/lightwallet.go
+++ chainntnfs/lightwalletnotify/lightwallet.go
@@ -192,7 +192,33 @@ out:
 	for {
 		select {
 		case cancelMsg := <-b.notificationCancels:
-			Mock(cancelMsg)
+			switch msg := cancelMsg.(type) {
+			case *epochCancel:
+				fmt.Printf("notificationCancels received")
+				chainntnfs.Log.Infof("Cancelling epoch "+
+					"notification, epoch_id=%v", msg.epochID)
+
+				// First, we'll lookup the original
+				// registration in order to stop the active
+				// queue goroutine.
+				reg := b.blockEpochClients[msg.epochID]
+				reg.epochQueue.Stop()
+
+				// Next, close the cancel channel for this
+				// specific client, and wait for the client to
+				// exit.
+				close(b.blockEpochClients[msg.epochID].cancelChan)
+				b.blockEpochClients[msg.epochID].wg.Wait()
+
+				// Once the client has exited, we can then
+				// safely close the channel used to send epoch
+				// notifications, in order to notify any
+				// listeners that the intent has been
+				// cancelled.
+				close(b.blockEpochClients[msg.epochID].epochChan)
+				delete(b.blockEpochClients, msg.epochID)
+
+			}
 
 		case registerMsg := <-b.notificationRegistry:
 			switch msg := registerMsg.(type) {
@@ -452,8 +478,6 @@ func (b *LightWalletNotifier) historicalConfDetails(confRequest chainntnfs.ConfR
 		// In the case that the filter exists, we'll attempt to see if
 		// any element in it matches our target public key script.
 		key := builder.DeriveKey(&reversed)
-		// TODO(yuraolex): fix it back after filters are properly built
-
 		match, err := filter.Match(key, confRequest.PkScript.Script())
 		if err != nil {
 			return nil, fmt.Errorf("unable to query filter: %v", err)
@@ -786,7 +810,7 @@ func (b *LightWalletNotifier) RegisterSpendNtfn(outpoint *wire.OutPoint,
 	// the outpoint has been spent. If it hasn't, we can return to the
 	// caller as well.
 	txOut, err := b.chainConn.GetUnspentOutput(
-		&spendRequest.OutPoint.Hash, int32(spendRequest.OutPoint.Index))
+		&spendRequest.OutPoint.Hash, spendRequest.OutPoint.Index)
 	if err != nil {
 		return nil, err
 	}
